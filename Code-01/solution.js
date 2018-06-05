318f2cf3cf20e3ddb2715e49a53ec18adf258dd4
AmandaMoen <amandalmoen@yahoo.com>
2018-06-05 16:13:32 -0700

    Created new directory, and cleaned up my code and files. Commented, and changed the naming convention slightly for my solution.

---
+++ Code-01/solution.js
@@ -0,0 +1,12 @@
+function abbrevName(name) {
+
+	// We've recently learned about using let in place of var.
+	// Split the names at the space.
+	let splitNames = name.split(' ');
+	// Grab the first letter of the first word/name.
+	let initalFirstName = splitNames[0][0];
+	// Grab the first letter of the second word/name.
+	let initialLastName = splitNames[1][0];
+	// We just learned about template literals.
+	return `${initialFirstName}.${initialLastName}`.toUpperCase();
+}
